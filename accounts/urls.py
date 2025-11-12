e7cfb6cd9492bea222b4f60bab8b335bfbbe171e
abdelmanem <74471687+abdelmanem@users.noreply.github.com>
2025-11-12 15:00:08 +0200

    commit

--- accounts/urls.py
+++ accounts/urls.py
@@ -19,5 +19,6 @@ urlpatterns = [
     path('users/create/', views.user_create, name='user_create'),
     path('users/<int:pk>/', views.user_detail, name='user_detail'),
     path('users/<int:pk>/update/', views.user_update, name='user_update'),
+    path('users/<int:pk>/reset-password/', views.user_reset_password, name='user_reset_password'),
     path('users/<int:pk>/delete/', views.user_delete, name='user_delete'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required, permission_required, user_passes_test
+from django.contrib.auth.forms import SetPasswordForm
 from django.db.models import Q, F, Value
 from django.db.models.functions import Concat
 from .models import CustomUser
@@ -291,6 +292,22 @@ def user_update(request, pk):
     else:
         form = UserUpdateForm(instance=user)
     return render(request, 'accounts/user_form.html', {'form': form, 'user': user})
+
+
+@login_required
+@permission_required('accounts.change_customuser', raise_exception=True)
+def user_reset_password(request, pk):
+    user = get_object_or_404(CustomUser, pk=pk)
+    if request.method == 'POST':
+        form = SetPasswordForm(user=user, data=request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Password reset successfully.')
+            return redirect('accounts:user_detail', pk=user.pk)
+        messages.error(request, 'Please correct the errors below.')
+    else:
+        form = SetPasswordForm(user=user)
+    return render(request, 'accounts/user_reset_password.html', {'form': form, 'user': user})
 
 @login_required
 @permission_required('accounts.delete_customuser', raise_exception=True)
